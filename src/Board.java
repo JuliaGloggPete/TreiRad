0882e7313c2933d24a0e4d3429456276e194a3ac
Julia Petersson <julia.gloggnitzer@outlook.com>
2022-09-29 11:45:40 +0200

    funktion if a spot is available is in place

--- src/Board.java
+++ src/Board.java
@@ -1,25 +1,26 @@
-public class Board  {
+public class Board {
 
-    String[] board= {".",".",".",".",".",".",".",".","."};
+    String[] board = {".", ".", ".", ".", ".", ".", ".", ".", "."};
 
     Player player;
+    String pos;
+
     public Board() {
-        this.board =board;
-
+        this.board = board;
 
 
     }
 
-    public void printBoard(){
-        this.board =board;
+    public void printBoard() {
+        this.board = board;
 
-        System.out.println("   1   2   3\n"+"a  "+board[0]+" | "+board[1]+" | "+board[2]+"\n"+
-                "  ---+---+---\n"+"b  "+board[3]+" | "+board[4]+" | "+board[5]+"\n"+
-                "  ---+---+---\n"+"c  "+board[6]+" | "+board[7]+" | "+board[8]+"\n");
+        System.out.println("   1   2   3\n" + "a  " + board[0] + " | " + board[1] + " | " + board[2] + "\n" +
+                "  ---+---+---\n" + "b  " + board[3] + " | " + board[4] + " | " + board[5] + "\n" +
+                "  ---+---+---\n" + "c  " + board[6] + " | " + board[7] + " | " + board[8] + "\n");
 
     }
 
-    public String []getBoard() {
+    public String[] getBoard() {
         return board;
     }
 
@@ -28,36 +29,79 @@ public class Board  {
 
     }
 
-    public boolean checkWin(){
-        this.player=player;
-        String h;
+    public boolean checkWin() {
+        this.player = player;
 
-        if (board[0].equals("X")&&board[1].equals("X")&&board[2].equals("X")) {
+
+        if (board[0].equals("X") && board[1].equals("X") && board[2].equals("X")) {
             System.out.println("Player1 you won");
         }
-        if (board[3].equals("X")&&board[4].equals("X")&&board[5].equals("X")) {
+        if (board[3].equals("X") && board[4].equals("X") && board[5].equals("X")) {
             System.out.println("Player1 you won");
         }
-        if (board[6].equals("X")&&board[7].equals("X")&&board[8].equals("X")) {
+        if (board[6].equals("X") && board[7].equals("X") && board[8].equals("X")) {
             System.out.println("Player1 you won");
         }
-        if (board[0].equals("X")&&board[3].equals("X")&&board[6].equals("X")) {
+        if (board[0].equals("X") && board[3].equals("X") && board[6].equals("X")) {
             System.out.println("Player1 you won");
         }
-        if (board[1].equals("X")&&board[4].equals("X")&&board[7].equals("X")) {
+        if (board[1].equals("X") && board[4].equals("X") && board[7].equals("X")) {
             System.out.println("Player1 you won");
         }
-        if (board[2].equals("X")&&board[5].equals("X")&&board[8].equals("X")) {
+        if (board[2].equals("X") && board[5].equals("X") && board[8].equals("X")) {
             System.out.println("Player1 you won");
         }
-        if (board[0].equals("X")&&board[4].equals("X")&&board[8].equals("X")) {
+        if (board[0].equals("X") && board[4].equals("X") && board[8].equals("X")) {
             System.out.println("Player1 you won");
         }
-        if (board[2].equals("X")&&board[4].equals("X")&&board[6].equals("X")) {
+        if (board[2].equals("X") && board[4].equals("X") && board[6].equals("X")) {
             System.out.println("Player1 you won");
         }
 
-return true;
+        return true;
     }
+    public boolean validMove(String pos){
 
+        this.pos= pos;
+
+        switch (pos) {
+            case "a1":
+                if (board[0] == ".") {return true;
+                }else {return false;}
+
+            case "a2":
+
+                if (board[1] == "."){return true;
+                }else {return false;}
+            case "a3":
+
+                if (board[2] == "."){return true;
+                } else {return false;}
+            case "b1":
+                if (board[3] == "."){return true;
+                }else {return false;}
+            case "b2":
+
+                if (board[4] == "."){return true;
+                } else {return false;}
+            case "b3":
+
+                if (board[5] == "."){return true;
+                } else {return false;}
+
+            case "c1":
+
+                if (board[6] == "."){return true;
+                } else {return false;}
+            case "c2":
+
+                if (board[7] == "."){return true;
+                } else {return false;}
+            case "c3":
+                if (board[8] == "."){return true;
+                } else {return false;}
+            default:
+                System.out.println("not a valid choice");
+        }return true;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -33,6 +33,8 @@ public class Main {
                 playerTurn();
 
 
+
+
             } while (play);
 
 
@@ -42,6 +44,7 @@ public class Main {
     public static void playerTurn() {
         Scanner sc = new Scanner(System.in);
         Board b = new Board();
+
         for (int i = 0; i < 9; i++) {
 
             if (i % 2 == 0) {
@@ -49,6 +52,7 @@ public class Main {
                 String choice = sc.nextLine();
 
                 String h= "X";
+                if (b.validMove(choice)==true){
                 switch (choice) {
                     case "a1":
 
@@ -95,57 +99,66 @@ public class Main {
                         b.board[8] = b.board[8].replace(".", h);
                         b.printBoard();
                         break;} b.checkWin();
-            } else {
+            } else{ System.out.println("not a valid move, now you are beeing skipped and it is player2 turn");  }}
+            else {
                 System.out.println("Player2 it's your turn, where do you want to set your O");
                 String choice = sc.nextLine();
 
-                String h= "O";
-            switch (choice) {
-                case "a1":
+                String h = "O";
+                if (b.validMove(choice) == true) {
+                    switch (choice) {
+                        case "a1":
 
-                    b.board[0] = b.board[0].replace(".", h);
-                    b.printBoard();
-                    break;
-                case "a2":
+                            b.board[0] = b.board[0].replace(".", h);
+                            b.printBoard();
+                            break;
+                        case "a2":
 
-                    b.board[1] = b.board[1].replace(".", h);
-                    b.printBoard();
-                    break;
-                case "a3":
+                            b.board[1] = b.board[1].replace(".", h);
+                            b.printBoard();
+                            break;
+                        case "a3":
 
-                    b.board[2] = b.board[2].replace(".", h);
-                    b.printBoard();
-                    break;
-                case "b1":
+                            b.board[2] = b.board[2].replace(".", h);
+                            b.printBoard();
+                            break;
+                        case "b1":
 
-                    b.board[3] = b.board[3].replace(".", h);
-                    b.printBoard();
-                    break;
-                case "b2":
+                            b.board[3] = b.board[3].replace(".", h);
+                            b.printBoard();
+                            break;
+                        case "b2":
 
-                    b.board[4] = b.board[4].replace(".", h);
-                    b.printBoard();
-                    break;
-                case "b3":
+                            b.board[4] = b.board[4].replace(".", h);
+                            b.printBoard();
+                            break;
+                        case "b3":
 
-                    b.board[5] = b.board[5].replace(".", h);
-                    b.printBoard();
-                    break;
-                case "c1":
+                            b.board[5] = b.board[5].replace(".", h);
+                            b.printBoard();
+                            break;
+                        case "c1":
 
-                    b.board[6] = b.board[6].replace(".", h);
-                    b.printBoard();
-                    break;
-                case "c2":
+                            b.board[6] = b.board[6].replace(".", h);
+                            b.printBoard();
+                            break;
+                        case "c2":
 
-                    b.board[7] = b.board[7].replace(".", h);
-                    b.printBoard();
-                    break;
-                case "c3":
+                            b.board[7] = b.board[7].replace(".", h);
+                            b.printBoard();
+                            break;
+                        case "c3":
 
-                    b.board[8] = b.board[8].replace(".", h);
-                    b.printBoard();
-                    break;} b.checkWin();
-            }                        
+                            b.board[8] = b.board[8].replace(".", h);
+                            b.printBoard();
+                            break;
+                    }
+                    b.checkWin();
+                } else {
+                    System.out.println("not a valid move, now you are beeing skipped and it is Player1*s turn");
+                }
+            }
+}}
 
-}}}
+
+}
