95ac046bb779f16a17ae5ce6097c7fe8b11ea182
Julia Petersson <julia.gloggnitzer@outlook.com>
2022-10-06 08:38:47 +0200

    Finally a working calculator for the wins is installed

--- src/Board.java
+++ src/Board.java
@@ -1,4 +1,4 @@
-import java.nio.channels.ScatteringByteChannel;
+
 import java.util.Arrays;
 
 public class Board {
@@ -22,7 +22,6 @@ public class Board {
     public boolean checkWin(String symbol) {
         this.symbol = symbol;
 
-
         if (board[0].equals(symbol) && board[1].equals(symbol) && board[2].equals(symbol) ||
                 board[3].equals(symbol) && board[4].equals(symbol) && board[5].equals(symbol) ||
                 board[6].equals(symbol) && board[7].equals(symbol) && board[8].equals(symbol) ||
@@ -37,44 +36,35 @@ public class Board {
         return false;
     }
 
+    public static int setPos(String pos) {
 
- /*   public int setPos(String pos) {
-
-        this.pos = pos;
-try{
-        switch (pos) {
-            case "a1":
-                return 0;
-            case "a2":
-                return 1;
-            case "a3":
-                return 2;
-            case "b1":
-                return 3;
-            case "b2":
-                return 4;
-            case "b3":
-                return 5;
-            case "c1":
-                return 6;
-            case "c2":
-                return 7;
-            case "c3":
-                return 8;
-        }} catch (Exception e){
-    System.out.println("not a vlid move");
-}
-
-// här behöver jag felhanteringen
+        try {
+            switch (pos) {
+                case "a1":
+                    return 0;
+                case "a2":
+                    return 1;
+                case "a3":
+                    return 2;
+                case "b1":
+                    return 3;
+                case "b2":
+                    return 4;
+                case "b3":
+                    return 5;
+                case "c1":
+                    return 6;
+                case "c2":
+                    return 7;
+                case "c3":
+                    return 8;
+            }
+        } catch (Exception e) {
+            System.out.println("not a valid move");
+        }
 
         return 9;
     }
-//    public ArrayIndexOutOfBoundsException(int index) {
-  //      super("Array index out of range: " + index);
-   // }
-//} */
-
-
 
     public boolean checkDraw(String[] board) {
 
@@ -88,7 +78,24 @@ try{
             return true;
         }
 
+    }
+    public static boolean validMove(String pos, Board b) {
 
+        try {
+            if (pos.equals("a1") || pos.equals("a2") || pos.equals("a3") ||
+                    pos.equals("b1") || pos.equals("b2") || pos.equals("b3") ||
+                    pos.equals("c1") || pos.equals("c2") || pos.equals("c3")) {
+
+                setPos(pos);
+
+                return b.board[setPos(pos)].equals(".");
+            }
+        } catch (Exception e) {
+            System.out.println("That's not a valid position");
+
+        }
+
+        return false;
     }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -2,109 +2,78 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
+
+
+
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
         Board b = new Board();
 
-do {
-    System.out.println("Hello - how about som Tic Tac Toe - Wanna play y/n?");
+        int won=0;
 
-    String wantToPlay = sc.nextLine();
+        System.out.println("Hello - how about som Tic Tac Toe - Wanna play? press anything and enter to continue");
+        sc.nextLine();
 
-    // kortar ner här genom if y if n och else not a valid answer och sen loopar om - då behöver jag inte tv
+        boolean play = true;
 
-    //validMenuChoice(wantToPlay);
-    switch (wantToPlay) {
-        case "n":
-            System.out.println("ok, bye!");
+        while (play) {
+            b.printBoard();
+            System.out.println("do you want to play against another player press 1 \n" +
+                    "do your want to play against the computer press 2");
+            String manOrMachine = sc.nextLine();
 
-            break;
-        case "y":
 
-            // 1 or two players
-            boolean play = true;
+            if (manOrMachine.equals("1")) {
 
-            while (play) {
-                b.printBoard();
-                System.out.println("do you want to play against another player press 1 \n" +
-                        "do your want to play against the computer press 2");
-                String manOrMachine = sc.nextLine();
-                // ska starta om utan att fråga igen...
+                System.out.println("Player 1 please insert your name");
+                Player p1 = new Player(sc.nextLine(), "X");
+                System.out.println("Player 2 please insert your name");
+                Player p2 = new Player(sc.nextLine(), "O");
 
-                if (manOrMachine.equals("1")) {
-                    System.out.println("Player 1 please insert your name");
-                    Player p1 = new Player(sc.nextLine(), "X");
-                    System.out.println("Player 2 please insert your name");
-                    Player p2 = new Player(sc.nextLine(), "O");
-                    playerTurn( p1, p2);
-                } else if (manOrMachine.equals("2")){
-                    System.out.println("Please insert your name");
-                    Player p1 = new Player(sc.nextLine(), "X");
-                    Player p2 = new Player("Megatron","Y");
-                    andrplayerTurn(p1, p2);
-                }
-            }
-        default:
-            System.out.println("not a valid answer");
-            break;
+                do {
+                    playerTurn(p1, p2);
 
-    }}
-    while (true) ;
-        /*} else {
-
-            String newWantToPlay;
-            do {
-                System.out.println("Not a vaild answer, do you want to play y/n");
-                newWantToPlay = sc.nextLine();
-
-            } while (!validMenuChoice(newWantToPlay));
-            if (validMenuChoice(newWantToPlay)) {
-
-                switch (newWantToPlay) {
-                    case "n":
+                    System.out.println("do you want to quit? Press q, wanna continue press anything else");
+                    String wantToQuit = sc.nextLine();
+                    if (wantToQuit.equals("q")) {
                         System.out.println("ok, bye!");
-
+                        play = false;
                         break;
-                    case "y":
-                        boolean play = true;
-
-                        while (play) {
-                            b.printBoard();
-                            System.out.println("do you want to play against another player press 1 \n" +
-                                    "do your want to play against the computer press 2");
-                            String manOrMachine= sc.nextLine();
-
-                            if (manOrMachine.equals("1")){
-
-                            playerTurn();}
-
-                        }
-                }*/
-
-        }
+                    }
+                }
+                while (play = true);
 
 
-    /*public static boolean validMenuChoice(String wantToPlay) {
+            } else if (manOrMachine.equals("2")) {
+                System.out.println("Please insert your name");
+                Player p1 = new Player(sc.nextLine(), "X");
+                Player p2 = new Player("Megatron", "O");
+                do {
 
-        while (true) {
-            if (wantToPlay.equals("y")) {
-                return true;
-            } else if (wantToPlay.equals("n")) {
-                return false;
+                    machineAndManPlayerTurn(p1, p2);
+
+                    System.out.println("do you want to quit? Press q, wanna continue press anything else");
+                    String wantToQuit = sc.nextLine();
+                    if (wantToQuit.equals("q")) {
+                        System.out.println("ok, bye!");
+                        play = false;
+                        break;
+                    }
+                }
+                while (play = true);
             }
-            return false;
         }
-    }*/
+    }
 
+    // should probably not have packed everything in player move at once - as I now have problems with the AI
     public static void playerMove(String choice, String symbol, String name, Board b) {
 
         Scanner sc = new Scanner(System.in);
 
-        if (validMove(choice, b)) {
-            b.board[setPos(choice)] = b.board[setPos(choice)].replace(".", symbol);
+        if (b.validMove(choice, b)) {
+            b.board[b.setPos(choice)] = b.board[b.setPos(choice)].replace(".", symbol);
             b.printBoard();
-
 
         } else {
             String newChoice;
@@ -115,49 +84,61 @@ do {
                         name + " it's your turn, where do you want to set your " + symbol + "!");
                 newChoice = sc.nextLine();
 
-            } while (!validMove(newChoice, b));
-            if (validMove(newChoice, b)) {
-                b.board[setPos(newChoice)] = b.board[setPos(newChoice)].replace(".", symbol);
+            } while (!b.validMove(newChoice, b));
+            if (b.validMove(newChoice, b)) {
+                b.board[b.setPos(newChoice)] = b.board[b.setPos(newChoice)].replace(".", symbol);
                 b.printBoard();
 
             }
         }
     }
 
+    public static void machineMove(String choice, String symbol, Board b) {
+
+        //if (validMove(choice,b))
+
+        if (b.validMove(choice, b)) {
+            b.board[b.setPos(choice)] = b.board[b.setPos(choice)].replace(".", symbol);
+            b.printBoard();
+        } else {
+
+            machineMove(artificialPlayer(b),
+                    symbol, b);
+        }
+    }
 
 
-
-public static String artificialPlayer(Board b) {
-        String symbol = "Y";
+    public static String artificialPlayer(Board b) {
+        String symbol = "O";
         Random random = new Random();
 
-        int robotPos = random.nextInt(8);
-            switch (robotPos) {
-                case 0:
-                    return "a1";
-                case 1:
-                    return "a2";
-                case 2:
-                    return "a3";
-                case 3:
-                    return "b1";
-                case 4:
-                    return "b2";
-                case 5:
-                    return "b3";
-                case 6:
-                    return "c1";
-                case 7:
-                    return "c2";
-                case 8:
-                    return "c3";
-            }
+        int robotPos = random.nextInt(9);
+        switch (robotPos) {
+            case 0:
+                return "a1";
+            case 1:
+                return "a2";
+            case 2:
+                return "a3";
+            case 3:
+                return "b1";
+            case 4:
+                return "b2";
+            case 5:
+                return "b3";
+            case 6:
+                return "c1";
+            case 7:
+                return "c2";
+            case 8:
+                return "c3";
+        }
 
+        return symbol;
 
-    return symbol;
-}
+    }
 
-    public static void andrplayerTurn(Player p1, Player p2) {
+    public static void machineAndManPlayerTurn(Player p1, Player p2) {
         Scanner sc = new Scanner(System.in);
         Board b = new Board();
 
@@ -171,7 +152,6 @@ public static String artificialPlayer(Board b) {
                 String symbol = p1.getSymbol();
                 String name = p1.getName();
                 playerMove(choice, symbol, name, b);
-                // artificialPlayer(b);
 
                 if (b.checkWin(symbol)) {
                     System.out.println(name + " you won!\nGet ready for a new round");
@@ -187,10 +167,9 @@ public static String artificialPlayer(Board b) {
                 artificialPlayer(b);
 
                 String symbol = "O";
-                String name = p2.getName();
-                playerMove(artificialPlayer(b), symbol, name, b);
+                machineMove(artificialPlayer(b), symbol, b);
 
-                if (b.checkWin(p2.getSymbol()) == true) {
+                if (b.checkWin(p2.getSymbol())) {
                     System.out.println(p2.getName() + " you won!\n get ready for a new round");
                     break;
                 } else {
@@ -200,10 +179,13 @@ public static String artificialPlayer(Board b) {
                     }
                 }
             }
-        }}
+        }
+    }
+
     public static void playerTurn(Player p1, Player p2) {
         Scanner sc = new Scanner(System.in);
         Board b = new Board();
+
 
         for (int i = 0; i < 9; i++) {
 
@@ -215,11 +197,19 @@ public static String artificialPlayer(Board b) {
                 String symbol = p1.getSymbol();
                 String name = p1.getName();
                 playerMove(choice, symbol, name, b);
-                // artificialPlayer(b);
+
 
                 if (b.checkWin(symbol)) {
+
+
+
+                    Xwon();
+                    //System.out.println(p1.getName()+"won"+ xWins);
                     System.out.println(name + " you won!\nGet ready for a new round");
+                    // System.out.println("total wins for "+p1.getName()+": "+ Xwin);
                     break;
+
+
                 } else {
 
                     if (b.checkDraw(b.board)) {
@@ -234,7 +224,9 @@ public static String artificialPlayer(Board b) {
                 String name = p2.getName();
                 playerMove(choice, symbol, name, b);
 
-                if (b.checkWin(p2.getSymbol()) == true) {
+                if (b.checkWin(p2.getSymbol())) {
+                    Owon();
+
                     System.out.println(p2.getName() + " you won!\n get ready for a new round");
                     break;
                 } else {
@@ -245,56 +237,20 @@ public static String artificialPlayer(Board b) {
                 }
             }
         }
+
+
+    }
+  static int Xwon= 0;
+    static int Owon= 0;
+    public static void Xwon(){
+
+        Xwon++;
+             System.out.println(Xwon);
     }
 
-    public static int setPos(String pos) {
+    public static void Owon(){
+        Owon++;
+        System.out.println(Owon);
 
-
-        try {
-            switch (pos) {
-                case "a1":
-                    return 0;
-                case "a2":
-                    return 1;
-                case "a3":
-                    return 2;
-                case "b1":
-                    return 3;
-                case "b2":
-                    return 4;
-                case "b3":
-                    return 5;
-                case "c1":
-                    return 6;
-                case "c2":
-                    return 7;
-                case "c3":
-                    return 8;
-            }
-        } catch (Exception e) {
-            System.out.println("not a valid move");
-        }
-
-// här behöver jag felhanteringen
-
-        return 9;
     }
-
-    public static boolean validMove(String pos, Board b) {
-
-
-        try {
-            if (pos.equals("a1") || pos.equals("a2") || pos.equals("a3") ||
-                    pos.equals("b1") || pos.equals("b2") || pos.equals("b3") ||
-                    pos.equals("c1") || pos.equals("c2") || pos.equals("c3")) {
-
-                setPos(pos);
-
-                return b.board[setPos(pos)].equals(".");
-            }} catch (Exception e) { System.out.println("Thats not a valid position");
-
-        }
-
-
-        return false;
-    }}
+}
